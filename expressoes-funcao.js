d936a932909f675b85c086cf2d3458ff9c51aa86
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-02-22 11:41:32 -0300

    criar arquivos de expressoes e funções para testes

---
+++ expressoes-funcao.js
@@ -0,0 +1,26 @@
+// declaração de função
+
+function minhaFuncao (param) {
+    // blooc de código
+}
+
+// minhaFuncao("param")
+
+// expressoa de função
+
+// const soma = function( num1, num2){return num1+num2}
+// console.log (soma(1,1))
+
+//diferença principal: HOISTING
+//Funções e var são "listadas" no topo do arquivo
+
+// diferença principal ????
+
+console.log(apresentar())
+
+function apresentar (){
+    return "olá";
+}
+
+console.log (soma(1, 1))
+const soma = function( num1, num2){return num1+num2}
